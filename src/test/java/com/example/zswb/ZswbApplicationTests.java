00343b0a07e49001b41d53ea3d8d57c9c4bf589b
958430327@qq.com <958430327@qq.com>
2020-05-07 17:07:53 +0800

    redis整合完成

--- src/test/java/com/example/zswb/ZswbApplicationTests.java
+++ src/test/java/com/example/zswb/ZswbApplicationTests.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.mail.SimpleMailMessage;
+
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.sql.DataSource;
@@ -25,6 +29,32 @@ class ZswbApplicationTests {
         connection.close();
 
     }
+    @Autowired
+   JavaMailSenderImpl mailSender;
 
+    @Test
+    public void email(){
+        //邮件设置1：一个简单的邮件
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setSubject("hhhh");
+        message.setText("今晚7:30开会");
+        message.setTo("958430327@qq.com");
+        message.setFrom("958430327@qq.com");
+        mailSender.send(message);
+    }
+
+
+    /**
+     * 测试redis
+     */
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void testRedis(){
+        redisTemplate.opsForValue().set("mykey","hh");
+        System.out.println((redisTemplate.opsForValue().get("mykey")));
+    }
 
 }
