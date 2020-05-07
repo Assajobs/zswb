c283d18615568cdeee070737ef7fa36cb5bed55a
958430327@qq.com <958430327@qq.com>
2020-05-07 09:31:49 +0800

    第一次模板

---
+++ src/main/java/com/example/zswb/ZswbApplication.java
@@ -0,0 +1,13 @@
+package com.example.zswb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZswbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZswbApplication.class, args);
+    }
+
+}
